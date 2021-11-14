b2df7772fa70cbf1d7b9ec120ab64013a43f575f
Brent Westbrook <brentrwestbrook@gmail.com>
2021-11-13 20:40:11 -0600

    extract time and hourly temperatures from XML

--- weather.lisp
+++ weather.lisp
@@ -16,3 +16,37 @@
   (xmls:parse
    (flexi-streams:octets-to-string
     (drakma:http-request *hourly-weather-url*))))
+
+(defparameter *weather* (get-weather))
+
+;; document -> data -> time-layout -> start-valid-time
+(defun extract-time (xml-weather-data)
+  "extract the datetime string from XML-WEATHER-DATA"
+  (mapcar #'(lambda (tag) (car (xmls:node-children tag)))
+	  (remove-if-not #'(lambda (n) (string-equal
+					(xmls:node-name n)
+					"start-valid-time"))
+			 (xmls:node-children
+			  (xmls:xmlrep-find-child-tag
+			   "time-layout"
+			   (xmls:xmlrep-find-child-tag "data" xml-weather-data))))))
+
+(defun extract-hourly-temp (xml-weather-data)
+  (mapcar #'(lambda (node)
+	      (read-from-string
+	       (car (xmls:node-children node))))
+	  (xmls:node-children
+	   (car
+	    (remove-if-not #'(lambda (x)
+			       (string-equal (xmls:xmlrep-attrib-value "type" x)
+					     "hourly"))
+			   (xmls:xmlrep-find-child-tags
+			    "temperature"
+			    (xmls:xmlrep-find-child-tag
+			     "parameters"
+			     (xmls:xmlrep-find-child-tag "data" xml-weather-data))))))))
+
+(let ((weather (get-weather)))
+  (mapcar #'list
+	  (extract-time weather)
+	  (extract-hourly-temp weather)))
